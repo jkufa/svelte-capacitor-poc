ac27e59496f5c49a897ef29d616b44ff47c3e091
Jack Kufa <jack@kufa.io>
2023-09-04 12:47:31 -0400

    Run through this tutorial: https://ionic.io/blog/cross-platform-sveltekit-capacitor-application-yes-its-possible

---
+++ app/src/routes/+page.svelte
@@ -0,0 +1,29 @@
+<script>
+  import { Geolocation } from '@capacitor/geolocation';
+
+  /**
+   * @type {{ coords: { latitude: any; longitude: any; }; } | null}
+   */
+  let loc = null;
+  async function getCurrentPosition(){
+      const res = await Geolocation.getCurrentPosition()
+      loc = res
+  }
+</script>
+
+<main>
+  <h1>Geolocation</h1>
+  <p>Your location is:</p>
+  <p>Latitude: {loc?.coords.latitude}</p>
+  <p>Longitude: {loc?.coords.longitude}</p>
+  
+  <button on:click={getCurrentPosition}>
+      Get Current Location
+  </button>
+</main>
+
+<style>
+  main {
+      padding-top: 50px;
+  }
+</style>
